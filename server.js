7dfba4b38b182322dbbbb9f002a05a2a8c280a9e
PawanK <tutor_pawan@hotmail.com>
2023-03-30 15:02:24 +0700

    test

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const PORT = process.env.PORT || 5001;
 
 const server = app.listen(
   PORT,
-  console.log("Server running in", process.env.NODE_ENV, "mode on port", PORT)
+  console.log(
+    "Server running in =>",
+    process.env.NODE_ENV,
+    "mode on port",
+    PORT
+  )
 );
 
 process.on("unhandledRejection", (err, promise) => {
